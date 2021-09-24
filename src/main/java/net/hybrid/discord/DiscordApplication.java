f560721b7369f6f0c5c46c7abcfcf61b2954bea3
liam <71061493+LiamHBest0608@users.noreply.github.com>
2021-09-24 16:48:29 +0200

    First

---
+++ src/main/java/net/hybrid/discord/DiscordApplication.java
@@ -0,0 +1,17 @@
+package net.hybrid.discord;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DiscordApplication extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
